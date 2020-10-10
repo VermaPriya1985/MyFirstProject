6473db1159251984f7e94f6cde49d6edc05830b6
Verma Saab <verpriya17@gmail.com>
2020-10-09 19:05:41 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
