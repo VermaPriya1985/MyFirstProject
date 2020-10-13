535fd4f1086063d5f812970285193e944cae56c8
Verma Saab <verpriya17@gmail.com>
2020-10-13 13:23:14 -0600

    Remove vowels from string

---
+++ RemoveVowels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic; 
+namespace MyFirstproject
+{
+    class RemoveVowels
+    {
+       public static void Run()
+        {
+            string newstr = "";
+            Console.WriteLine("Enter String to remove vowels");
+            string str = Console.ReadLine();
+            for(int i=0;i<str.Length;i++)
+            {
+                if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u')
+                {
+                    newstr += str[i];
+                }
+            }
+            Console.WriteLine("The New String is: " + newstr);
+        }
+    }
+}
