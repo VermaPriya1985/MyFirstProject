fc596637475a04eeaacd5ee10e1e3fb6f93713e2
Verma Saab <verpriya17@gmail.com>
2020-10-13 12:58:36 -0600

    Sum of all nos between 2 nos

---
+++ SumNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace MyFirstproject
+{
+    class SumNumber
+    {
+        public static void Run()
+        {
+            
+           try  
+           {
+
+            double sum = 0;
+            double diff = 0;
+            Console.WriteLine("Enter first number");
+            double first_num=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            double second_num=Convert.ToDouble(Console.ReadLine());
+            diff = second_num-first_num;
+            for(int i=0;i<=diff;i++)
+            {
+                 
+                  sum = first_num + sum;        
+                  first_num = first_num + 1; 
+                  // Console.WriteLine("The Sum is: " + sum);
+                  // Console.WriteLine("The first_num is: " + first_num);
+             } 
+            Console.WriteLine("The Sum is: " + sum);
+           }
+           catch(Exception ex)
+           {
+
+               Console.WriteLine("Kindly provide correct value");
+           }
+        }
+    }
+}
