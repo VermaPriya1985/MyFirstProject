6ca96aac3e4fc488dde8217dd409d9b5a8c3c240
Verma Saab <verpriya17@gmail.com>
2020-10-12 22:41:57 -0600

    List of city names

--- CityNames.cs
+++ CityNames.cs
@@ -7,12 +7,26 @@ namespace MyFirstproject
        // static void Main(string[] args)
        public static void Run()
         {
-            // FizzBuzz.Run();
-            // LongestSequence.Run();
-           // DecimalBinary.Run();
-           // Console.WriteLine("hello");
-           
-            // Dynamic Size in List
+             List<string> cities = new List<string>();
+
+            // Gather input
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Please enter a city name: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput.Length >= 3 && userInput.Length <= 30) {
+                    cities.Add(userInput);
+                } else {
+                    Console.WriteLine("Invalid city length");
+                }
+            }
+
+            // Print the list
+            Console.WriteLine("-----------------------");
+            for (int i = 0; i < cities.Count; i++) 
+            {
+                Console.WriteLine(cities[i]);
+            }
         }
     }
 }
